de818922cd36d6dbdf67539910d9d403e1fc3988
dhivyamanikanda <dhivyamanikandan1@gmail.com>
2025-03-02 11:55:20 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 const jsonData = JSON.parse(process.argv[2]);
 console.log('Inside js Parsed JSON Data:', jsonData);
 
-Object.keys(jsonData).forEach(key => {
-  const nestedObject = jsonData[key];
-  console.log('Key:', key);
-  console.log('Value:', nestedObject);
-  // Access nested properties as needed
-});
+// Object.keys(jsonData).forEach(key => {
+//   const nestedObject = jsonData[key];
+//   console.log('Key:', key);
+//   console.log('Value:', nestedObject);
+//   // Access nested properties as needed
+// });
 
-// Access the parsed JSON object
-console.log(jsonData);
+// // Access the parsed JSON object
+// console.log(jsonData);
 
 console.log('Username (gb):', jsonData.gb.username);
 console.log('Password (de):', jsonData.de.password);
