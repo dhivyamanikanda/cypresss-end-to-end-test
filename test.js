6ad8020733d273f44ed8b887407eb4a8dde06dc6
dhivyamanikanda <dhivyamanikandan1@gmail.com>
2025-03-02 11:02:29 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
-const jsonData = JSON.parse(process.argv[2]);
+const jsonData = JSON.parse(process.argv[1]);
+// const jsonData = JSON.parse(process.argv[2]);
 console.log('Parsed JSON Data:', jsonData);
