80f5aded0af3a89e7c13d4350a6f4c7a3f5bef1b
dhivyamanikanda <dhivyamanikandan1@gmail.com>
2025-03-02 11:37:42 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const jsonData = JSON.parse(process.argv[2]);
-console.log('Parsed JSON Data:', jsonData);
+console.log('Inside js Parsed JSON Data:', jsonData);
 
 jsonData.forEach(secret => {
   console.log('Username:', secret.username);
