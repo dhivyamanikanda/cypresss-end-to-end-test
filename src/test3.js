146f79d9b47a58cb5f2cb5e1a1f81460fc75930d
dhivyamanikanda <dhivyamanikandan1@gmail.com>
2025-03-07 15:50:40 +0100

    Update test3.js

--- src/test3.js
+++ src/test3.js
@@ -1,5 +1,4 @@
 const jsonData = JSON.parse(process.env.JSON_DATA);
-console.log('Inside js Parsed JSON Data:', jsonData);
 
 console.log('Username (de):', jsonData.de.username);
 console.log('Password (de):', jsonData.de.password);
